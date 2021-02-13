245c8ab8d715a611638c8c599260d4230cb73c8a
edgarancioto <junior.ancioto@gmail.com>
2021-02-13 13:17:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def functions():
 
 def main():
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 if __name__ == "__main__":
     main()
