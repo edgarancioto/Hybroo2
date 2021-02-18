ff07a03ea08fbd71c49f36629409c21b17e4d63c
edgarancioto <junior.ancioto@gmail.com>
2021-02-18 13:40:53 -0300

    Adjust

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request
 from flask_cors import CORS
 import json
+from sympy import latex, sympify
 
 app = Flask(__name__)
 
@@ -21,15 +22,24 @@ def functions_details():
     function_id = int(request.args.get('id'))
     file_data = open(os.path.dirname(__file__) + "/BackEnd/FunctionProblem/Functions/functions-details.json", 'r')
     data = json.loads(file_data.read())
-    print()
-    print(data)
-    print(type(data))
-    print(data['data'])
     for i in data['data']:
         if function_id == i['id']:
             return i
     return "<h1>NOT FOUND</h1>"
-     
+
+
+@app.route('/functions-details-img')
+def functions_details_img():
+    function_id = int(request.args.get('id'))
+    file_data = open(os.path.dirname(__file__) + "/BackEnd/FunctionProblem/Functions/functions-details.json", 'r')
+    data = json.loads(file_data.read())
+    for i in data['data']:
+        if function_id == i['id']:
+            func = sympify(i['formulation'], evaluate=False)
+            return 'http://latex.codecogs.com/svg.latex?'+latex(func)
+    return "<h1>NOT FOUND</h1>"
+        
+
 
 def main():
     port = int(os.environ.get("PORT", 5000))
