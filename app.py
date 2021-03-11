30ddb63b0f7e7652934b523b309b58a5a5b9f76e
edgarancioto <junior.ancioto@gmail.com>
2021-03-11 13:12:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,8 +136,6 @@ def main():
 if __name__ == "__main__":
     main()"""
 
-
-
     
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -163,5 +161,6 @@ def message(a):
     my_event('response')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app,host="0.0.0.0", port=port, debug=True)
 
