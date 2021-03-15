66b20d651bae6d733a30b67cb881fbd57dbfe2db
edgarancioto <junior.ancioto@gmail.com>
2021-03-15 20:31:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+import sys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -17,6 +18,7 @@ def index():
 @app.route('/functions-names')
 def api():
     print('api()')
+    sys.stdout.flush()
     query = dict(request.args)
     socketio.emit('log', dict(data=str(query)), broadcast=True)
     return jsonify(dict(success=True, message='Received'))
@@ -25,10 +27,12 @@ def api():
 @socketio.on('connect')
 def on_connect():
     print('on_connect()')
+    sys.stdout.flush()
     payload = dict(data='Connected')
     emit('log', payload, broadcast=True)
 
 
 if __name__ == '__main__':
     print('main')
+    sys.stdout.flush()
     socketio.run(app)
