009db8bfd0e2986fb8b426d3372068714670c8a3
edgarancioto <junior.ancioto@gmail.com>
2021-03-17 16:21:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import websockets
 connected = set()
 
 def functions_names():
-    file_data = open(os.path.dirname(__file__) + "/CODE/JSON/functions-names.json", 'r')
+    file_data = open(os.path.dirname(__file__) + "/HYBROO2/CODE/JSON/functions-names.json", 'r')
     return json.loads(file_data.read())
 
 async def server(websocket, path):
