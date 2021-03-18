81e6eb39685951088449266f203a1d975735b60c
edgarancioto <junior.ancioto@gmail.com>
2021-03-17 21:07:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,31 +8,9 @@ import websockets
 
 connected = set()
 
-opc = 1
-
 def functions_names():
-    global opc
-    print(opc)
-    if opc == 1:
-        opc = 2
-        return try_opc("../HYBROO2/CODE/JSON/functions-names.json")
-    if opc == 2:
-        opc = 3
-        return try_opc("../CODE/JSON/functions-names.json")
-    if opc == 3:
-        opc = 4
-        return try_opc("/CODE/JSON/functions-names.json")
-    if opc == 4:
-        opc = 1
-        return try_opc("./CODE/JSON/functions-names.json")
-    
-
-def try_opc(string):
-    try:
-        file_data = open(string, 'r')
-        return json.loads(file_data.read())
-    except FileNotFoundError:
-        return string
+    file_data = open("./CODE/JSON/functions-names.json", 'r')
+    return json.loads(file_data.read())
 
 async def server(websocket, path):
     connected.add(websocket) 
